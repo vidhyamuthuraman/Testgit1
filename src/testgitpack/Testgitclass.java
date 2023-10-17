9fa1acc3eec5fdd6256b5cc347af8662af6b343f
vidhyamuthuraman <124411822+vidhyamuthuraman@users.noreply.github.com>
2023-10-17 14:34:58 -0500

    Update Testgitclass.java

--- src/testgitpack/Testgitclass.java
+++ src/testgitpack/Testgitclass.java
@@ -1,15 +1,12 @@
 package testgitpack;
 
 public class Testgitclass {
-<<<<<<< HEAD
+
 public static void main(String args[])
 {
 	System.out.println("Git tested");
 }
-=======
-  public static voin main(String args[])
-  {
-  }
 
->>>>>>> 8d53eef806b7411ae2b7c612c47913c6b7d6a7a8
+ 
+
 }
